2bfff5462f915d96e712b0db5392ab0edbf4dc72
Vazi 华子 <vazi@vazi.cn>
2016-08-06 22:53:37 +0800

    第一次代码提交

---
+++ EquicksWeb/EquicksWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EquicksWeb.Startup))]
+namespace EquicksWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
